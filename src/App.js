d5c8e7774530fc14c9ef9b746db8a08ef1709a5b
Sauravkumar1232 <kumarsaurav11494@gmail.com>
2023-04-09 22:37:53 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
+import dayjs from "dayjs";
+import DatePicker from'react-date-picker';
 
 function App() {
+  curDT: new Date().toLocaleString()
+  const curr = new Date();
+  console.log(curr.getTime);
+
   const [data, setData] = useState({});
   const [location, setLocation] = useState("Delhi");
+  
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ec46e9a52c98f9c142eae8d1fe9febc5`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=ec46e9a52c98f9c142eae8d1fe9febc5`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
@@ -25,33 +32,43 @@ function App() {
             onChange={(event) => setLocation(event.target.value)}
             placeholder="Enter Location"
             onKeyDown={searchLocation}
-            type="text"
+            type="search"
           />
         </div>
         <div className="container">
           <div className="top">
             <div className="location">
-              <p>{data.name}</p>
+              <p>{data.name},</p> {data.sys? <p>{data.sys.country}</p>:null}
             </div>
             <div className="temp">
-              {data.main ? <h1>{data.main.temp}</h1> : null}
+              {data.main ? <h1>{data.main.temp}°C</h1> : null}
             </div>
             <div className="discription">
-              {data.weahter ? <p>{data.weather[0].main}</p> : null}
-              
+              {data.weather ? <p>{data.weather[0].description}</p> : null}
+            </div>
+            <div className="date">
+              {/* {data.dt & data.timezone ? (
+                <p>{(new Date(data.dt*1000-(data.timezone*1000)))}</p>
+              ) : "not found"} */
+              }
+              {/* <DatePicker onChange={onChange} value={value}/> */}
+              {/* <p>Current Date And Time : {this.state.curDT}</p> */}
+
+              <p>{console.log(curr.getTime)}</p>
             </div>
           </div>
           <div className="bottom">
             <div className="feels">
-            {data.main ? <p>{data.main.feels_like}</p> : null}
+              {data.main ? <p>{data.main.feels_like}°C</p> : null}
               <p className="bold">Feels Like</p>
             </div>
             <div className="humidity">
-              <p>20%</p>
+              {data.main ? <p>{data.main.humidity}%</p> : null}
               <p className="bold">Humidity</p>
             </div>
             <div className="wind">
-              <p>12MPH</p>
+              {data.wind ? <p>{data.wind.speed} km/h</p> : null}
+
               <p className="bold">Wind Speed</p>
             </div>
           </div>
